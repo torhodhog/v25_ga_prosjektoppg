0ff8dae3c646f1178771816a65ff3f3a829fdbac
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2025-03-19 13:21:09 +0100

    Pasdientside laget

---
+++ v25_ga_prosjektoppgave/src/app/admin/pasienter/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function PasientPage() {
+   return (
+      <>
+      <MaxWidthWrapper>
+     <div className="max-w-3xl mx-auto py-10">
+       <h1 className="text-2xl font-bold text-center">Her er alle dine pasienter</h1>
+     </div>
+     </MaxWidthWrapper>
+     </>
+   );
+ }
+ 
